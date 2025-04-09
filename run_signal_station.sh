974261df9de9b71b40b56b813f7bbc21e6d5dd3d
Gabriel le Roux <lrxgab005@gmail.com>
2025-04-09 17:28:24 +0200

    Added kodi folder name config file

--- run_signal_station.sh
+++ run_signal_station.sh
@@ -34,6 +34,15 @@ else
     exit 1
 fi
 
+# Try to get Kodi folder from config file
+if [ -f "config/kodi_folder_name.conf" ]; then
+    KODI_FOLDER=$(cat config/kodi_folder_name.conf)
+    echo "Using Kodi folder from config: $KODI_FOLDER"
+else
+    echo "Error: No Kodi folder config found. Please create config/kodi_folder_name.conf"
+    exit 1
+fi
+
 # Kill any existing session
 tmux kill-session -t $SESSION_NAME 2>/dev/null || true
 
@@ -52,7 +61,7 @@ PANES=($(tmux list-panes -F "#{pane_id}"))
 # Auto-restart loops in each pane:
 tmux send-keys -t ${PANES[0]} "$VENV_ACTIVATE && while true; do python3 $SERIAL_SCRIPT --udp_send_targets 127.0.0.1:$AUDIO_BIND_PORT 127.0.0.1:$KODI_BIND_PORT --udp_listen_targets 127.0.0.1:$AUDIO_SEND_PORT --serial_port $ARDUINO_PORT; echo \"[$(date)] $SERIAL_SCRIPT crashed. Restarting in 1 second...\"; sleep 1; done" $CM
 tmux send-keys -t ${PANES[1]} "$VENV_ACTIVATE && while true; do python3 $AUDIO_SCRIPT --udp_bind_port $AUDIO_BIND_PORT --udp_send_port $AUDIO_SEND_PORT; echo \"[$(date)] $AUDIO_SCRIPT crashed. Restarting in 1 second...\"; sleep 1; done" $CM
-tmux send-keys -t ${PANES[2]} "$VENV_ACTIVATE && while true; do python3 $KODI_SCRIPT --udp_bind_port $KODI_BIND_PORT --ip $ANDROID_IP; echo \"[$(date)] $KODI_SCRIPT crashed. Restarting in 1 second...\"; sleep 1; done" $CM
+tmux send-keys -t ${PANES[2]} "$VENV_ACTIVATE && while true; do python3 $KODI_SCRIPT --udp_bind_port $KODI_BIND_PORT --ip $ANDROID_IP --dir $KODI_FOLDER; echo \"[$(date)] $KODI_SCRIPT crashed. Restarting in 1 second...\"; sleep 1; done" $CM
 
 # Don't attach if running from systemd (no TTY)
 if [ -t 1 ]; then
